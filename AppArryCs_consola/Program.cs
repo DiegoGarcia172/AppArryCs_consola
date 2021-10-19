74747c5a1f0678f11673854b5833c26b99895003
DIEGO <DIEGO@DESKTOP-VLF2ESU>
2021-10-19 12:29:41 -0500

    Agregar archivos de proyecto.

---
+++ AppArryCs_consola/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppArryCs_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prueba[] ArregloPrueba = new prueba[5];
+
+            for (int i = 0; i < ArregloPrueba.Length; i++)
+            {
+
+
+                ArregloPrueba[i] = new prueba();
+                Console.WriteLine("Cual es tu nombre? ");
+
+                string nombre = Convert.ToString(Console.ReadLine());
+
+                ArregloPrueba[i].nombre = nombre;
+
+                Console.WriteLine("en que escuela estas? ");
+
+                string escuela = Convert.ToString(Console.ReadLine());
+
+
+                ArregloPrueba[i].escuela = escuela;
+
+                Console.WriteLine("Cual es la fecha del dia de hoy? ");
+
+                string f = Convert.ToString(Console.ReadLine());
+
+                ArregloPrueba[i].Fecha = DateTime.Now;
+
+                Console.WriteLine("Que carrera cursas? ");
+
+                string C = Convert.ToString(Console.ReadLine());
+
+                ArregloPrueba[i].carrera = C;
+
+            }
+
+
+
+            for (int i = 0; i < ArregloPrueba.Length; i++)
+            {
+
+                Console.WriteLine(ArregloPrueba[i].ToString());
+
+            }
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ AppArryCs_consola/prueba.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppArryCs_consola
+{
+    class prueba
+    {
+
+        private DateTime? fecha;
+
+        public DateTime? Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        private string Escuela;
+
+        public string escuela
+        {
+            get { return Escuela; }
+            set { Escuela = value; }
+        }
+
+        private string Nombre;
+
+        public string nombre
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+
+        private string Carrera;
+
+        public string carrera
+        {
+            get { return Carrera; }
+            set { Carrera = value; }
+        }
+
+
+
+        public prueba() 
+        {
+            Nombre = "";
+            Escuela = "";
+            Fecha = null ;
+            Carrera = "";
+
+        }
+
+
+        public override string ToString()
+        {
+            return Nombre + " " + Escuela + " " + Fecha + " " + Carrera + "";
+
+        }
+
+
+
+
+
+
+    }
+}
